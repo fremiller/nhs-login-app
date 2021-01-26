87812146fb8e30c1af59c2c0e966e7ed1a54f5ed
Freddie Miller <fred.miller097@gmail.com>
2021-01-26 11:58:28 +0000

    modules: Create NhsLogin module

---
+++ components/NhsLogin.ts
@@ -0,0 +1,52 @@
+import { AuthConfiguration, authorize } from 'react-native-app-auth';
+
+const possibleScopes = [
+    'openid',
+    'profile',
+    'profile_extended',
+    'email',
+    'phone',
+    'gp_registration_details',
+    'gp_integration_credentials',
+    'client_metadata'
+]
+
+export class NhsLogin {
+    config: AuthConfiguration = {
+        issuer: 'https://auth.sandpit.signin.nhs.uk',
+        clientId: 'du-nhs-login',
+        redirectUrl: 'com.nhsloginapp://oauth',
+        scopes: ['openid'],
+        skipCodeExchange: true,
+        dangerouslyAllowInsecureHttpRequests: true,
+    }
+
+    async NhsLoginAuthorise() {
+        let result = await authorize(this.config).catch((e) => {
+            console.log(e);
+        });
+        if (!result){
+            return;
+        }
+        console.log(`Authorization code: ${result.authorizationCode}`);
+        await this.AuthorizationCodeExchange(result.authorizationCode);
+    }
+
+    async AuthorizationCodeExchange(authCode: string){
+
+    }
+
+    GetScopes(): {
+        name: string,
+        enabled: boolean,
+        disabled: boolean
+    }[] {
+        return possibleScopes.map((scope)=> {
+            return {name: scope, enabled: this.config.scopes.indexOf(scope) != -1, disabled: scope == 'openid'}
+        })
+    }
+
+    SetScopes(scopes: string[]){
+        this.config.scopes = scopes;
+    }
+}
